7fe4db536f4958d9d5401b561b18093b24247887
n0vabyte <od3tocode@protonmail.com>
2024-06-20 11:03:01 -0400

    rework deploy and run

--- scripts/run.sh
+++ scripts/run.sh
@@ -89,7 +89,7 @@ function ansible:build {
   uuid: ${UUID}
   # sudo user
   sudo_username: ${SUDO_USERNAME}
-  username: ${SUDO_USERNAME}
+  #username: ${SUDO_USERNAME}
   cluster_size: ${CLUSTER_SIZE}
   # db user
   db_user: ${DB_USER}
@@ -114,6 +114,64 @@ function ansible:build {
 EOF
 }
 
+# controller temp sshkey
+function controller_sshkey {
+    ssh-keygen -o -a 100 -t ed25519 -C "ansible" -f "${HOME}/.ssh/id_ansible_ed25519" -q -N "" <<<y >/dev/null
+    export ANSIBLE_SSH_PUB_KEY=$(cat ${HOME}/.ssh/id_ansible_ed25519.pub)
+    export ANSIBLE_SSH_PRIV_KEY=$(cat ${HOME}/.ssh/id_ansible_ed25519)
+    export SSH_KEY_PATH="${HOME}/.ssh/id_ansible_ed25519"
+    chmod 700 ${HOME}/.ssh
+    chmod 600 ${SSH_KEY_PATH}
+    eval $(ssh-agent)
+    ssh-add ${SSH_KEY_PATH}
+}
+
+# build instance vars before cluster deployment
+function build {
+  local CASSANDRA_VERSION="${CASSANDRA_VERSION}"
+  local LINODE_PARAMS=($(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .label,.type,.region,.image))
+  local LINODE_TAGS=$(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .tags)
+  local GROUP_VARS="${WORK_DIR}/group_vars/apache-cassandra/vars"
+  local TEMP_ROOT_PASS=$(openssl rand -base64 32)
+  controller_sshkey
+
+  cat << EOF >> ${group_vars}
+# user vars
+sudo_username: ${SUDO_USERNAME}
+api_token: ${TOKEN_PASSWORD}
+
+# deployment vars
+uuid: ${UUID}
+ssh_keys: ${ANSIBLE_SSH_PUB_KEY}
+instance_prefix: ${INSTANCE_PREFIX}
+type: ${LINODE_PARAMS[1]}
+region: ${LINODE_PARAMS[2]}
+image: ${LINODE_PARAMS[3]}
+linode_tags: ${LINODE_TAGS}
+root_pass: ${TEMP_ROOT_PASS}
+
+# cassandra vars
+cluster_name: ${CLUSTER_NAME}
+db_user: ${DB_SUPERUSER}
+cassandra_version: ${CASSANDRA_VERSION}
+cluster_size: ${CLUSTER_SIZE}
+client_count: ${CLIENT_COUNT}
+
+
+# ssl/tls
+country_name: ${COUNTRY_NAME}
+state_or_province_name: ${STATE_OR_PROVINCE_NAME}
+locality_name: ${LOCALITY_NAME}
+organization_name: ${ORGANIZATION_NAME}
+email_address: ${EMAIL_ADDRESS}
+ca_common_name: ${CA_COMMON_NAME}
+EOF
+}
+
+function deploy { 
+    for playbook in provision.yml site.yml; do ansible-playbook -v -i hosts $playbook; done
+}
+
 function ansible:deploy {
   ansible-playbook -v provision.yml
   ansible-playbook -v -i hosts site.yml --extra-vars "root_password=${ROOT_PASS} add_keys_prompt=${ADD_SSH_KEYS}"
@@ -121,6 +179,6 @@ function ansible:deploy {
 
 # main
 case $1 in
-    ansible:build) "$@"; exit;;
-    ansible:deploy) "$@"; exit;;
+    build) "$@"; exit;;
+    deploy) "$@"; exit;;
 esac
